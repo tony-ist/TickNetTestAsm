e729c2f22c3177c477a7e890a4d9a5bf8d0d8623
Tony Istomin <istomanton@gmail.com>
2025-12-20 18:47:21 +0400

    feat: add halt

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,14 @@ semantics.addOperation('assemble', {
   programLine: programLine => programLine.assemble(),
   instructionLine: (_inlineSpace1, instruction, _inlineSpace2, _inlineComment, _newline) => instruction.assemble(),
   instruction: (mnemonic, _space1, port, _space2, data) => {
+    if (mnemonic.sourceString === 'halt') {
+      return 0;
+    }
+
     const checkBit = mnemonic.sourceString === 'check' ? 1 : 0;
     const portInt = parseInt(port.sourceString);
     const dataInt = parseInt(data.sourceString);
-    return checkBit + (portInt << 1) + (dataInt << 3);
+    return checkBit + (portInt << 1) + (dataInt << 4);
   },
   _iter: (...children) => children.map(c => c.assemble()).filter(x => x !== null),
 });
@@ -37,4 +41,9 @@ if (matchResult.failed()) {
 
 const assembled = semantics(matchResult).assemble();
 console.log(assembled);
-console.log(assembled.map((x: number) => x.toString(2)).join(' '));
+console.log(assembled.map((x: number) => padWithZeros(x.toString(2), 12)).join(' '));
+console.log(assembled.map((x: number) => padWithZeros(x.toString(16).toUpperCase(), 4)).join(' '));
+
+function padWithZeros(value: string, length: number) {
+  return value.padStart(length, '0');
+}
