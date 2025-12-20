b9a4713968480352b2045a3643461cea19e142fc
Anton Istomin <istomanton@gmail.com>
2025-12-20 16:44:30 +0400

    chore: update readme

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as ohm from 'ohm-js';
 import * as fs from 'fs';
 import * as path from 'path';
-import simpleTns from './programs/simple.tns?raw';
 
 const grammarSource = fs.readFileSync(
   path.join(__dirname, '..', 'src', 'grammar.ohm'),
@@ -9,5 +8,33 @@ const grammarSource = fs.readFileSync(
 );
 const grammar = ohm.grammar(grammarSource);
 
-const simpleProgram = grammar.trace(simpleTns);
-console.log(simpleProgram);
+const simpleTns = fs.readFileSync(
+  path.join(__dirname, 'programs', 'simple.tns'),
+  'utf-8'
+);
+
+const semantics = grammar.createSemantics();
+
+semantics.addOperation('assemble', {
+  program: programLines => programLines.assemble(),
+  programLine: programLine => programLine.assemble(),
+  instructionLine: (_inlineSpace1, instruction, _inlineSpace2, _inlineComment, _newline) => instruction.assemble(),
+  instruction: (mnemonic, _space1, port, _space2, data) => {
+    const checkBit = mnemonic.sourceString === 'check' ? 1 : 0;
+    const portInt = parseInt(port.sourceString);
+    const dataInt = parseInt(data.sourceString);
+    return checkBit + (portInt << 1) + (dataInt << 3);
+  },
+  _iter: (...children) => children.map(c => c.assemble()).filter(x => x !== null),
+});
+
+const matchResult = grammar.match(simpleTns);
+
+if (matchResult.failed()) {
+  console.error(matchResult.message);
+  process.exit(1);
+}
+
+const assembled = semantics(matchResult).assemble();
+console.log(assembled);
+console.log(assembled.map((x: number) => x.toString(2)).join(' '));
